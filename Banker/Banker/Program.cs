be6605e373c4e78eba990025ac77b79522294690
SteveHentschel <slhcoding@gmail.com>
2014-01-29 08:23:45 -0500

    A simple example of unit testing on a console banker app from MSDN. Good starter concepts.  slh

---
+++ Banker/Banker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banker
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
